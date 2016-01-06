e73395bd581b8cd86a9827d58a41a3fed113eab7
danmo91 <dan.morain91@gmail.com>
2016-01-06 16:37:07 -0700

    initial commit

---
+++ ImageEditor.java
@@ -0,0 +1,15 @@
+import services.Pixel;
+
+public class ImageEditor {
+
+  public static void main(String [] args) {
+
+    // load image
+
+    // perform transformation
+
+    // save image
+
+  }
+
+}
---
+++ services/Pixel.java
@@ -0,0 +1,15 @@
+package services;
+
+public class Pixel {
+
+  public int red;
+  public int green;
+  public int blue;
+
+  public void print () {
+    System.out.println("(" + red + "," + green + "," + blue + ")");
+    
+  }
+
+
+}
