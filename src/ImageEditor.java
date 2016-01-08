f34c5504cb229342cc934ae7f7aeaebbf6fa57b4
danmo91 <dan.morain91@gmail.com>
2016-01-08 12:03:29 -0700

    use switch/case on tranform image

--- src/ImageEditor.java
+++ src/ImageEditor.java
@@ -6,48 +6,32 @@ import java.io.File;
 
 public class ImageEditor {
 
-  // public Image load_image (String [] args) {
-  //
-  //
-  //
-  //   try {
-  //
-  //     File srcFile = new File(args[0]);
-  //     Scanner scanner = new Scanner(srcFile);
-  //     image = FileHandler.parse_file(scanner);
-  //     scanner.close();
-  //
-  //   }
-  //   catch (Exception e) {
-  //     System.out.println("exception => " + e);
-  //     System.out.println("USAGE: java ImageEditor in-file out-file (grayscale|invert|emboss|motionblur motion-blur-length)");
-  //   }
-  //   finally {
-  //     return image;
-  //   }
-  //
-  // }
-
   public Image transform_image (Image image, String [] args) {
 
     // invert, grayscale, emboss, motionblur
-
     try {
       // get transformation type
       String transformation = args[2];
 
       // transform image
-      if (transformation.equals("invert")) {
-        Effects.invert(image);
-      } else if (transformation.equals("grayscale")) {
-        Effects.grayscale(image);
-      } else if (transformation.equals("emboss")) {
-        Effects.emboss(image);
-      } else if (transformation.equals("motionblur")) {
-        int motion_blur_length = Integer.valueOf(args[3]);
-        Effects.motionblur(image, motion_blur_length);
-      }
+      switch(transformation) {
+        case "invert":
+            Effects.invert(image);
+            break;
+        case "grayscale":
+            Effects.grayscale(image);
+            break;
+        case "emboss":
+            Effects.emboss(image);
+            break;
+        case "motionblur":
+            int motion_blur_length = Integer.valueOf(args[3]);
+            Effects.motionblur(image, motion_blur_length);
+            break;
+        default:
+            throw new Exception("invalid tranformation => " + transformation);
 
+      }
 
     } catch (Exception e) {
       System.out.println("Exception => " + e);
