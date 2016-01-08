69986ee60fdf00fc91116452f8bcdb3300b1b8d5
danmo91 <dan.morain91@gmail.com>
2016-01-08 11:30:08 -0700

    created Effects.java to hold transformation functions

--- src/ImageEditor.java
+++ src/ImageEditor.java
@@ -1,5 +1,6 @@
 import imageEditor.Image;
 import imageEditor.Pixel;
+import imageEditor.Effects;
 import java.util.Scanner;
 import java.io.File;
 import java.io.PrintWriter;
@@ -100,147 +101,6 @@ public class ImageEditor {
 
   }
 
-  public void invert (Image image) {
-    // inverted_value = max_color_value - original_value
-    for (int row = 0; row < image.height; row++) {
-      for (int col = 0; col < image.width; col++) {
-        // get pixel
-        Pixel pixel = image.pixels[row][col];
-        // transform each color
-        pixel.red = image.max_color_value - pixel.red;
-        pixel.green = image.max_color_value - pixel.green;
-        pixel.blue = image.max_color_value - pixel.blue;
-        // save
-        image.pixels[row][col] = pixel;
-      }
-    }
-
-    return;
-  }
-
-  public void grayscale(Image image) {
-
-    // new_value = (red + green + blue)/3
-    for (int row = 0; row < image.height; row++) {
-      for (int col = 0; col < image.width; col++) {
-        // get pixel
-        Pixel pixel = image.pixels[row][col];
-        // transform each color
-        int gray_color = (pixel.red + pixel.green + pixel.blue)/3;
-        pixel.red = gray_color;
-        pixel.green = gray_color;
-        pixel.blue = gray_color;
-        // save
-        image.pixels[row][col] = pixel;
-      }
-    }
-
-
-    return;
-  }
-
-  public void emboss(Image image) {
-
-    // for each pixel
-    for (int row = image.height-1; row > 0; row--) {
-      for (int col = image.width-1; col > 0; col--) {
-
-        // get pixel
-        Pixel pixel = image.pixels[row][col];
-        int emboss_value = 0;
-        int maxDiff = 0;
-
-        // calculate emboss value
-        if (row-1 >= 0 && col-1 >= 0) {
-
-          // get color diffs
-          int redDiff = pixel.red - image.pixels[row-1][col-1].red;
-          int greenDiff = pixel.green - image.pixels[row-1][col-1].green;
-          int blueDiff = pixel.blue - image.pixels[row-1][col-1].blue;
-
-          // find max diff
-          maxDiff = redDiff;
-          if (Math.abs(maxDiff) < Math.abs(greenDiff)) {
-            maxDiff = greenDiff;
-          } else if (Math.abs(maxDiff) < Math.abs(blueDiff)) {
-            maxDiff = blueDiff;
-          }
-
-          // set emboss value
-          emboss_value = 128 + maxDiff;
-
-          // scale emboss value
-          if (emboss_value < 0) {
-            emboss_value = 0;
-          } else if (emboss_value > 255) {
-            emboss_value = 255;
-          }
-
-        } else {
-          emboss_value = 128;
-        }
-
-        // transform each color
-        pixel.red = emboss_value;
-        pixel.green = emboss_value;
-        pixel.blue = emboss_value;
-
-        // save
-        image.pixels[row][col] = pixel;
-
-      }
-    }
-    return;
-  }
-
-  public void motionblur(Image image, int motion_blur_length) {
-
-    try {
-      // check valid blur length
-      if (motion_blur_length <= 0) {
-        throw new Exception("invalid motion blur length => " + motion_blur_length );
-      }
-      // get average for each color
-      for (int row = 0; row < image.height; row++) {
-        for (int col = 0; col < image.width; col++) {
-          // get pixel
-          Pixel pixel = image.pixels[row][col];
-
-          // get sum of each color
-          int red_sum = 0;
-          int green_sum = 0;
-          int blue_sum = 0;
-          int actual_length = 1;
-          for (int i = col; (i < col + motion_blur_length - 1) && (i < image.width); i++) {
-            red_sum += image.pixels[row][i].red;
-            green_sum += image.pixels[row][i].green;
-            blue_sum += image.pixels[row][i].blue;
-            actual_length++;
-          }
-
-          // calculate average
-          int red_average = red_sum/actual_length;
-          int green_average = green_sum/actual_length;
-          int blue_average = blue_sum/actual_length;
-
-          // transform each color
-          pixel.red = red_average;
-          pixel.green = green_average;
-          pixel.blue = blue_average;
-
-          // save
-          image.pixels[row][col] = pixel;
-        }
-      }
-
-
-    } catch (Exception e) {
-      System.out.println("Exception => " + e);
-    } finally {
-      return;
-    }
-  }
-
   public Image transform (Image image, String [] args) {
 
     // invert, grayscale, emboss, motionblur
@@ -251,14 +111,14 @@ public class ImageEditor {
 
       // transform image
       if (transformation.equals("invert")) {
-        invert(image);
+        Effects.invert(image);
       } else if (transformation.equals("grayscale")) {
-        grayscale(image);
+        Effects.grayscale(image);
       } else if (transformation.equals("emboss")) {
-        emboss(image);
+        Effects.emboss(image);
       } else if (transformation.equals("motionblur")) {
         int motion_blur_length = Integer.valueOf(args[3]);
-        motionblur(image, motion_blur_length);
+        Effects.motionblur(image, motion_blur_length);
       }
 
 
@@ -321,7 +181,7 @@ public class ImageEditor {
     // create ImageEditor object
     ImageEditor imageEditor = new ImageEditor();
 
-    // load image into memory
+    // load image
     Image original_image = imageEditor.load_image(args);
 
     // perform transformation
---
+++ src/ImageEditor/Effects.java
@@ -0,0 +1,146 @@
+package imageEditor;
+
+public class Effects {
+
+  public static void invert (Image image) {
+    // inverted_value = max_color_value - original_value
+    for (int row = 0; row < image.height; row++) {
+      for (int col = 0; col < image.width; col++) {
+        // get pixel
+        Pixel pixel = image.pixels[row][col];
+        // transform each color
+        pixel.red = image.max_color_value - pixel.red;
+        pixel.green = image.max_color_value - pixel.green;
+        pixel.blue = image.max_color_value - pixel.blue;
+        // save
+        image.pixels[row][col] = pixel;
+      }
+    }
+
+    return;
+  }
+
+  public static void grayscale(Image image) {
+
+    // new_value = (red + green + blue)/3
+    for (int row = 0; row < image.height; row++) {
+      for (int col = 0; col < image.width; col++) {
+        // get pixel
+        Pixel pixel = image.pixels[row][col];
+        // transform each color
+        int gray_color = (pixel.red + pixel.green + pixel.blue)/3;
+        pixel.red = gray_color;
+        pixel.green = gray_color;
+        pixel.blue = gray_color;
+        // save
+        image.pixels[row][col] = pixel;
+      }
+    }
+
+
+    return;
+  }
+
+  public static void emboss(Image image) {
+
+    // for each pixel
+    for (int row = image.height-1; row > 0; row--) {
+      for (int col = image.width-1; col > 0; col--) {
+
+        // get pixel
+        Pixel pixel = image.pixels[row][col];
+        int emboss_value = 0;
+        int maxDiff = 0;
+
+        // calculate emboss value
+        if (row-1 >= 0 && col-1 >= 0) {
+
+          // get color diffs
+          int redDiff = pixel.red - image.pixels[row-1][col-1].red;
+          int greenDiff = pixel.green - image.pixels[row-1][col-1].green;
+          int blueDiff = pixel.blue - image.pixels[row-1][col-1].blue;
+
+          // find max diff
+          maxDiff = redDiff;
+          if (Math.abs(maxDiff) < Math.abs(greenDiff)) {
+            maxDiff = greenDiff;
+          } else if (Math.abs(maxDiff) < Math.abs(blueDiff)) {
+            maxDiff = blueDiff;
+          }
+
+          // set emboss value
+          emboss_value = 128 + maxDiff;
+
+          // scale emboss value
+          if (emboss_value < 0) {
+            emboss_value = 0;
+          } else if (emboss_value > 255) {
+            emboss_value = 255;
+          }
+
+        } else {
+          emboss_value = 128;
+        }
+
+        // transform each color
+        pixel.red = emboss_value;
+        pixel.green = emboss_value;
+        pixel.blue = emboss_value;
+
+        // save
+        image.pixels[row][col] = pixel;
+
+      }
+    }
+    return;
+  }
+
+  public static void motionblur(Image image, int motion_blur_length) {
+
+    try {
+      // check valid blur length
+      if (motion_blur_length <= 0) {
+        throw new Exception("invalid motion blur length => " + motion_blur_length );
+      }
+      // get average for each color
+      for (int row = 0; row < image.height; row++) {
+        for (int col = 0; col < image.width; col++) {
+          // get pixel
+          Pixel pixel = image.pixels[row][col];
+
+          // get sum of each color
+          int red_sum = 0;
+          int green_sum = 0;
+          int blue_sum = 0;
+          int actual_length = 1;
+          for (int i = col; (i < col + motion_blur_length - 1) && (i < image.width); i++) {
+            red_sum += image.pixels[row][i].red;
+            green_sum += image.pixels[row][i].green;
+            blue_sum += image.pixels[row][i].blue;
+            actual_length++;
+          }
+
+          // calculate average
+          int red_average = red_sum/actual_length;
+          int green_average = green_sum/actual_length;
+          int blue_average = blue_sum/actual_length;
+
+          // transform each color
+          pixel.red = red_average;
+          pixel.green = green_average;
+          pixel.blue = blue_average;
+
+          // save
+          image.pixels[row][col] = pixel;
+        }
+      }
+
+
+    } catch (Exception e) {
+      System.out.println("Exception => " + e);
+    } finally {
+      return;
+    }
+  }
+
+}
